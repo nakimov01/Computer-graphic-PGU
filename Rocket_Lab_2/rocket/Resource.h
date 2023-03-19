3266aca08c58d15e016a43eaef16cf6f9895249e
nakimov01 <nikita2003akimov@bk.ru>
2023-03-19 22:57:56 +0300

    Start Lab 2

---
+++ Rocket_Lab_2/rocket/Resource.h
@@ -0,0 +1,36 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется rocket.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_ROCKET_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+
+#define IDM_MOVE				50
+#define IDM_MIRROR				51
+#define IDM_SCALE				52
+#define IDM_ORIGIN				53
+#define IDI_ROCKET			107
+#define IDI_SMALL				108
+#define IDC_ROCKET			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+
+#endif
+// Следующие стандартные значения для новых объектов
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
